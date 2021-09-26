dad02ff908224a62f305b9b8cbca9e9ef344e83b
milliwonkim <rldnjs9347@gmail.com>
2021-09-26 16:14:19 +0900

    recoil with react

---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+import { useRecoilValue } from 'recoil';
+import { filteredTodoListState } from '../store';
+import TodoItem from './TodoItem';
+import TodoItemCreator from './TodoItemCreator';
+import TodoListFilters from './TodoListFilters';
+import TodoListStats from './TodoListStats';
+
+function TodoList() {
+    // changed from todoListState to filteredTodoListState
+    const todoList = useRecoilValue(filteredTodoListState);
+
+    return (
+        <>
+            <TodoListStats />
+            <TodoListFilters />
+            <TodoItemCreator />
+
+            {todoList.map((todoItem) => (
+                <TodoItem item={todoItem} key={todoItem.id} />
+            ))}
+        </>
+    );
+}
+
+export default TodoList;
